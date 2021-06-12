74d74a485d79792729bbe009170eb6f9a78015d0
Tai@Kuntzler <48732698+taiane18@users.noreply.github.com>
2021-06-11 21:54:21 -0300

    Add files via upload

---
+++ algoritmos/aula2.php
@@ -0,0 +1,42 @@
+<?php
+//OPERADORES
+	/*
+	== igual
+	=== idêntico
+	!= diferente
+	!== não idêntico
+	< menor que
+	> maior que
+	*/
+
+	/*
+	$valor = 10;
+	$valor += 5;
+	//echo $valor;
+	*/
+
+	/*
+	$valor1 = 15;
+	$valor2 = 10;
+	//var_dumb($valor1 >== $valor2);
+
+	($valor1 == $valor2) ? $resultado = true : $resultado = false;
+	//var_dump($resultado);
+	*/
+
+	/*
+	$v1 = 10;
+	$v2 = $v1++;
+	//echo($v1. ' - ' .$v2);
+	//echo $v1;
+	*/
+
+	/*
+	$v = true;
+	$f = false;
+	//var_dump(!($f));
+	*/
+
+	$nome = 'Taiane Maíra';
+	$sobrenome = ' Kuntzler';
+	echo $nome.$sobrenome;
